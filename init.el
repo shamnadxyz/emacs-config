73d72be074912b3233ef79a38dc2c6b9b6ed4532
shamnadxyz <shamnadxyz@proton.me>
2026-05-15 14:23:53 +0000

    feat: use melpa stable and use default configs

--- init.el
+++ init.el
@@ -1,18 +1,13 @@
 ; Add melpa package-archive
 (require 'package)
 (add-to-list 'package-archives
-             '("melpa" . "https://melpa.org/packages/") t)
-
-(setq package-archive-priorities
-      '(("gnu" . 20)
-        ("non-gnu" . 10)
-        ("melpa" . 0)))
+             '("melpa-stable" . "https://stable.melpa.org/packages/") t)
 
 (package-initialize)
 
-; Set JetBrainsMono as the default font
+; Set Iosevka as the default font
 (add-to-list 'default-frame-alist
-             '(font . "Iosevka NF-14"))
+             '(font . "Iosevka NF-16"))
 
 ;; Remove GUI components
 (menu-bar-mode -1)
@@ -27,9 +22,10 @@
 (setq org-log-done 'time)
 (setq vc-follow-symlinks t)
 
-; (load-theme 'modus-operandi :no-confirm)
-(load-theme 'modus-vivendi :no-confirm)
+(load-theme 'modus-operandi :no-confirm)
+;; (load-theme 'modus-vivendi :no-confirm)
 
+;; Vim emulation
 (use-package evil
   :ensure t
   :init
@@ -46,23 +42,36 @@
   :ensure t
   :config
   (evil-collection-init))
- 
+
+;; Vertico - Minibuffer.
 (use-package vertico
   :ensure t
-  :config
-  (vertico-mode 1))
-
-(use-package orderless
-  :ensure t
   :custom
-  (completion-styles '(orderless basic))
-  (completion-category-overrides '((file (styles partial-completion))))
-  (completion-pcm-leading-wildcard t)) ;; Emacs 31: partial-completion behaves like substring
+  (vertico-count 15) ;; Show more candidates
+  :init
+  (vertico-mode))
 
-(use-package magit
-  :ensure t
-  :bind ("C-c g s" . magit-status))
+;; Persist history over Emacs restarts. Vertico sorts by history position.
+(use-package savehist
+  :init
+  (savehist-mode))
 
+;; Emacs minibuffer configurations.
+(use-package emacs
+  :custom
+  ;; Enable context menu. `vertico-multiform-mode' adds a menu in the minibuffer
+  ;; to switch display modes.
+  (context-menu-mode t)
+  ;; Support opening new minibuffers from inside existing minibuffers.
+  (enable-recursive-minibuffers t)
+  ;; Hide commands in M-x which do not work in the current mode.  Vertico
+  ;; commands are hidden in normal buffers. This setting is useful beyond
+  ;; Vertico.
+  (read-extended-command-predicate #'command-completion-default-include-p)
+  ;; Do not allow the cursor in the minibuffer prompt
+  (minibuffer-prompt-properties
+   '(read-only t cursor-intangible t face minibuffer-prompt)))
+ 
 (use-package company
   :ensure t
   :hook (after-init . global-company-mode)
@@ -74,7 +83,14 @@
   (define-key company-active-map (kbd "C-y")
     #'company-complete-selection))
 
-(use-package yasnippet
+ (use-package orderless
+  :ensure t
+  :custom
+  (completion-styles '(orderless basic))
+  (completion-category-overrides '((file (styles partial-completion))))
+  (completion-pcm-leading-wildcard t)) ;; Emacs 31: partial-completion behaves like substring
+
+ (use-package yasnippet
   :ensure t
   :custom
   (yas-global-mode 1))
@@ -86,6 +102,9 @@
 (use-package markdown-mode
   :ensure t
   :hook (markdown-mode . eglot-ensure))
+
+(use-package magit
+  :ensure t)
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
